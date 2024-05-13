35ba99542b8b72f4a840ac6fa106d3129c18b4e7
Daceit <matthew.sullivan23@student.cart.org>
2024-05-13 15:49:30 -0700

    Flip

---
+++ CONTENT/Script.js
@@ -0,0 +1,12 @@
+function FunnySwitch() {
+    let A = document.getElementById("Sign");
+    let B = document.getElementById(`img`)
+    let C = document.getElementById(`Sign2`)
+    A.classList.toggle("Flip");
+    B.classList.toggle(`ProjectImgDino`)
+    C.classList.toggle(`Flip`)
+}
+// function ImgChange(){
+//     let B = document.getElementById(`img`);
+//     B.classList.toggle(`ProjectImgDino`)
+// }
