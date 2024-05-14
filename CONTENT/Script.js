72aced264c5bfb281b01bdc38554dd475662bfaa
Daceit <matthew.sullivan23@student.cart.org>
2024-05-14 14:44:20 -0700

    AAAAAAAAA

--- CONTENT/Script.js
+++ CONTENT/Script.js
@@ -1,15 +1,31 @@
-const b = 0
 
+let count = 0;
 function FunnySwitch() {
+    count++;
+    // Front
+    if ((count % 2) === 0){
+      let B = document.getElementById(`ProjectImg`);
+      console.log(count)
+      B.classList.toggle(`ProjectImgCalc`);
+    } 
+    // back
+    if ((count % 2) !== 0){
+      let B = document.getElementById(`ProjectImg2`);
+      console.log(count)
+      B.classList.toggle(`ProjectImgDino`);
+    } 
+    if ((count % 2) !== 0){
+      let B = document.getElementById(`ProjectImg`);
+      console.log(count)
+      B.classList.toggle(`ProjectImgCalc`);
+    } 
+    // Front
     let A = document.getElementById("Sign");
-    let B = document.getElementById(`ProjectImg`);
+    // let B = document.getElementById(`ProjectImg2`);
     let C = document.getElementById(`Sign2`);
     A.classList.toggle("Flip");
-    // B.classList.toggle(`ProjectImgDino`);
+    // B.classList.toggle(`ProjectImgDino`)
     C.classList.toggle(`Flip`);
-    for (let i = 0; i < i + 1; i++) {
-        console.log(i)
-      }
 
 }
 
